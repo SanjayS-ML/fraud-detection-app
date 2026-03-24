fe351dfa04f52700aa86c64c97c17635c5dad2ec
SANJAY.S <150994789+SanjayS-ML@users.noreply.github.com>
2026-03-24 10:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ vectorizer = pickle.load(open('vectorizer.pkl','rb'))
 
 st.set_page_config(page_title='Fraud Detector', layout='centered')
 st.title('Fraud Detection App')
+st.markdown('### 🔍 Detect whether a message is Fraud or Not")
 st.write('Enter a message to check whether it is Fraud or Not')
 
-user_input = st.text_area('Enter Text')
+#example button
+if st.button('Try Example'):
+    user_input = 'Congratualtions! You've won a free prize. Click now!'
+else:
+    user_input = st.text_area('Enter Text')
 
 if st.button('Predict'):
     if user_input.strip() == "":
-        st.warning('Please enter some text')
+        st.warning('⚠️Please enter some text')
     else:
         #Transform input 
         input_data = vectorizer.transform([user_input])
@@ -24,7 +29,26 @@ if st.button('Predict'):
         prediction = 1 if prob > threshold else 0 
 
         if prediction ==1:
-            st.error(f'Fraud Detected (Confidence: {prob:.2f})')
+            st.error(f'⚠️Fraud Detected ')
+            st.write(f'Confidence Score: {prob:.2f}')
         else:
-            st.success(f'Not Fraud (Confidence: {1-prob:.2f})')
+            st.success(f'✅Not Fraud ')
+            st.write(f'Confidence: {1-prob:.2f}')
+
+# Divider 
+st.markdown('----')
+
+#Info Section 
+st.markdown('### ℹ️ About this App')
+st.write(''' This application uses Machine Learning to detect fraudulent messages.
+
+- Model: Random Forest Classifier  
+- Text Processing: TF-IDF Vectorization  
+- Threshold tuning applied for better fraud detection  
+
+Built using Streamlit for real-time predictions.
+""")
+
+#warning 
+st.warning('⚠️ This is a demo ML model and may not be 100% accurate.')
 
